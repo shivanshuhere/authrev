70bc2959c5d3861e22113bfbaecaff9b65bafc8c
Shivanshu <ks7876555@gmail.com>
2025-02-14 20:22:00 +0530

    add flowbite footer , tailwind header and main

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-// export const plugins = [require("tailwindcss"), require("autoprefixer")];
-
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export const plugins = [tailwindcss, autoprefixer];
