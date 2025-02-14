dcc76c0346b7f6e17527d3c16d643726a07822f8
Shivanshu <ks7876555@gmail.com>
2025-02-14 19:35:26 +0530

    install tailwind , postcss

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+// export const plugins = [require("tailwindcss"), require("autoprefixer")];
+
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export const plugins = [tailwindcss, autoprefixer];
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
