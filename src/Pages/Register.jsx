fdd538b8f450d93808c16ea344e3168e9a99726e
Shivanshu <ks7876555@gmail.com>
2025-02-20 09:23:26 +0530

    accept changes

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -5,34 +5,46 @@ import { useDispatch, useSelector } from "react-redux";
 import { register } from "../Store/userSlice.js";
 
 export default function Register() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    username: "",
-  });
-  const [profile, setProfile] = useState(null);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        username: "",
+    });
+    const [profile, setProfile] = useState(null);
 
-  const user = useSelector((state) => state.user.value);
-  const dispatch = useDispatch();
+    const user = useSelector((state) => state.user.value);
+    const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = new FormData();
-    data.append("username", formData.username);
-    data.append("email", formData.email);
-    data.append("password", formData.password);
-<<<<<<< HEAD
-    data.append("profile", profile);
-
-    const handelFileChange = (event) => {
-        setProfile(event.target.value);
-    };
-
-    const hanldeSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        const data = new FormData();
+        data.append("username", formData.username);
+        data.append("email", formData.email);
+        data.append("password", formData.password);
+        if (profile) {
+            data.append("profile", profile);
+        }
+
         try {
-        } catch (error) {}
+            const res = await axios.post(
+                "http://localhost:5000/api/register",
+                data,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            console.log(res);
+            dispatch(register(res.data));
+            alert("Registered successfully!");
+        } catch (error) {
+            alert("Failed to register, error: " + JSON.stringify(error));
+            console.log("Failed to register, error: ", JSON.stringify(error));
+        }
     };
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -43,16 +55,10 @@ export default function Register() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form
-                        className="space-y-6"
-                        method="POST"
-                        action={"http://localhost:5000/api/register"}
-                        encType="multipart/form-data"
-                        // onSubmit={hanldeSubmit}
-                    >
+                    <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
                             <label
-                                htmlFor="email"
+                                htmlFor="username"
                                 className="block text-sm/6 font-medium text-gray-900"
                             >
                                 Username
@@ -64,12 +70,12 @@ export default function Register() {
                                     type="text"
                                     required
                                     autoComplete="username"
-                                    onChange={(e) => {
+                                    onChange={(e) =>
                                         setFormData({
                                             ...formData,
                                             username: e.target.value,
-                                        });
-                                    }}
+                                        })
+                                    }
                                     value={formData.username}
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
@@ -89,12 +95,12 @@ export default function Register() {
                                     type="email"
                                     required
                                     autoComplete="email"
-                                    onChange={(e) => {
+                                    onChange={(e) =>
                                         setFormData({
                                             ...formData,
                                             email: e.target.value,
-                                        });
-                                    }}
+                                        })
+                                    }
                                     value={formData.email}
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
@@ -116,12 +122,12 @@ export default function Register() {
                                     name="password"
                                     type="password"
                                     required
-                                    onChange={(e) => {
+                                    onChange={(e) =>
                                         setFormData({
                                             ...formData,
                                             password: e.target.value,
-                                        });
-                                    }}
+                                        })
+                                    }
                                     value={formData.password}
                                     autoComplete="current-password"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -130,7 +136,7 @@ export default function Register() {
                         </div>
                         <div>
                             <label
-                                htmlFor="email"
+                                htmlFor="profile"
                                 className="block text-sm/6 font-medium text-gray-900"
                             >
                                 Profile picture
@@ -141,158 +147,39 @@ export default function Register() {
                                     name="profile"
                                     type="file"
                                     required
-                                    onChange={handelFileChange}
-                                    value={profile}
+                                    accept="image/*"
+                                    onChange={(e) =>
+                                        setProfile(e.target.files[0])
+                                    }
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
-=======
-    if (profile) {
-      data.append("profile", profile);
-    }
 
-    try {
-      const res = await axios.post("http://localhost:5000/api/register", data, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
 
-      console.log(res);
-      dispatch(register(res.data));
-      alert("Registered successfully!");
-    } catch (error) {
-      alert("Failed to register, error: " + JSON.stringify(error));
-      console.log("Failed to register, error: ", JSON.stringify(error));
-    }
-  };
->>>>>>> d9fd647b728afd902c7aaabb30be82cd816a2635
-
-  return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Create account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  required
-                  autoComplete="username"
-                  onChange={(e) =>
-                    setFormData({ ...formData, username: e.target.value })
-                  }
-                  value={formData.username}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+                    <p className="mt-10 text-center text-sm/6 text-gray-500">
+                        Already a member?{" "}
+                        <Link
+                            to={"/login"}
+                            className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        >
+                            Login here
+                        </Link>
+                    </p>
+                </div>
+                <div>
+                    <p>User : {JSON.stringify(user)}</p>
+                </div>
             </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                  value={formData.email}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                  value={formData.password}
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="profile"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Profile picture
-              </label>
-              <div className="mt-2">
-                <input
-                  id="profile"
-                  name="profile"
-                  type="file"
-                  required
-                  accept="image/*"
-                  onChange={(e) => setProfile(e.target.files[0])}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Already a member?{" "}
-            <Link
-              to={"/login"}
-              className="font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              Login here
-            </Link>
-          </p>
-        </div>
-        <div>
-          <p>User : {JSON.stringify(user)}</p>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
